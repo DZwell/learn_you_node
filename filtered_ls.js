11fbe9d8e27e1071149f4234fb3fe22339b0cbfd
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-08-04 10:41:55 -0700

    Put var in front of files in for loop. It's not Python

--- filtered_ls.js
+++ filtered_ls.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var my_files = fs.readdir(process.argv[2], function(err, files){
     if (err) throw err;
-    for (file in files) {
+    for (var file in files) {
         if (path.extname(files[file]) === '.' + process.argv[3]) {
             console.log(files[file]);
         }
