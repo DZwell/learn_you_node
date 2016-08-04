19ebf280370fe3eabf8900826d260f630e7d11c1
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-08-04 10:40:17 -0700

    Exercise 5

---
+++ filtered_ls.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+var path = require('path');
+var my_files = fs.readdir(process.argv[2], function(err, files){
+    if (err) throw err;
+    for (file in files) {
+        if (path.extname(files[file]) === '.' + process.argv[3]) {
+            console.log(files[file]);
+        }
+    }
+});
